d68d4f3f313ee9d943b946aa8a819073b3b41b63
RoryEiffe <roryeiffe8@gmail.com>
2022-09-13 12:23:34 -0400

    Added CORS fix

--- src/main/java/com/revature/controller/PetController.java
+++ src/main/java/com/revature/controller/PetController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.nio.file.Path;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/pets")
 public class PetController {
